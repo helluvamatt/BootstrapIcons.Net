c631ba4516fbeab86edf0fecec8290006edcdd65
Matt Schneeberger <helluvamatt@gmail.com>
2021-10-20 04:40:47 -0500

    Add (simple) demo application

---
+++ BootstrapIcons.Wpf.Demo/DemoViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace BootstrapIcons.Wpf.Demo
+{
+    public class DemoViewModel : DependencyObject
+    {
+        public static readonly DependencyProperty SelectedIconProperty = DependencyProperty.Register(nameof(SelectedIcon), typeof(BootstrapIconGlyph), typeof(DemoViewModel), new PropertyMetadata(BootstrapIconGlyph.None));
+
+        public BootstrapIconGlyph SelectedIcon
+        {
+            get => (BootstrapIconGlyph)GetValue(SelectedIconProperty);
+            set => SetValue(SelectedIconProperty, value);
+        }
+
+        public IEnumerable<BootstrapIconGlyph> Glyphs => Enum.GetValues<BootstrapIconGlyph>();
+    }
+}
